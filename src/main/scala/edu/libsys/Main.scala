71f798929b43b240c46e286c096ccbb9ef94b2e0
yz <yziyz@foxmail.com>
2017-03-09 21:38:42 +0800

    Update

--- src/main/scala/edu/libsys/Main.scala
+++ src/main/scala/edu/libsys/Main.scala
@@ -5,10 +5,10 @@ import org.apache.spark.sql.SparkSession
 object Main {
 
   //lend record
-  case class histRecord(cert_id: Int, time: String, record_no: Int, call_no: String)
+  case class HistRecord(cert_id: Int, time: String, record_no: Int, call_no: String)
 
   //item
-  case class item(prop_no: Int, record_no: Int, count: Int)
+  case class Item(prop_no: Int, record_no: Int, count: Int)
 
   //main method
   def main(args: Array[String]): Unit = {
@@ -16,13 +16,28 @@ object Main {
       .builder()
       .appName("BookStatistics")
       .getOrCreate()
-    //load files
-    val item = spark.sparkContext.textFile("/home/spark/Project/data/csv/ITEM.csv")
-    val book = spark.sparkContext.textFile("/home/spark/Project/data/csv/ASORD_MARC.csv")
+    //load data
+    val item = spark.sparkContext.textFile("/home/spark/Project/data/csv/item.csv")
+    val lend_hist = spark.sparkContext.textFile("/home/spark/Project/data/csv/lend_hist.csv")
     //cache data
     item.cache()
-    book.cache()
+    lend_hist.cache()
+    //test
+    val histRecord = parseHistRecord(lend_hist.first())
 
+    //stop
     spark.stop()
+  }
+
+  //parse String to histRecord
+  def parseHistRecord(line: String) = {
+    //replace """(comma)
+    val pieces = line.replaceAll("\"", "").split(",")
+    println(line.replaceAll("\"", ""))
+    val cert_id = pieces(0).toInt
+    val time = pieces(1)
+    val record_no = pieces(2).toInt
+    val call_no = pieces(3)
+    HistRecord(cert_id, time, record_no, call_no)
   }
 }
---
+++ src/main/scala/edu/libsys/Util.java
@@ -0,0 +1,34 @@
+package edu.libsys;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Util {
+    public static void main(String[] args) {
+        long l = Time.stringToTimestamp("2000-09-0209:54:56");
+        System.out.println(l);
+        System.out.println(Time.timestampToString(l));
+    }
+
+}
+
+class Time {
+    public static String timestampToString(long time) {
+        return new java.text
+                .SimpleDateFormat("yyyy-MM-ddHH:mm:ss")
+                .format(new java.util.Date(time * 1000));
+    }
+
+    public static long stringToTimestamp(String s) {
+        try {
+            return new SimpleDateFormat("yyyy-MM-ddHH:mm:ss").parse(s).getTime() / 1000;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static void getTime() {
+        System.out.println(timestampToString(System.currentTimeMillis() / 1000));
+    }
+}
