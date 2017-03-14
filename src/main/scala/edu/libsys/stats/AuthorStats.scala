863647a9b0928431992d1f9377f5f866407ab2f6
yz <yziyz@foxmail.com>
2017-03-14 22:43:19 +0800

    Complete AuthorStats.

--- src/main/scala/edu/libsys/stats/AuthorStats.scala
+++ src/main/scala/edu/libsys/stats/AuthorStats.scala
@@ -1,18 +1,10 @@
 package edu.libsys.stats
 
-import com.google.common.hash.BloomFilter
 import edu.libsys.data.dao.AuthorDao
-import edu.libsys.dataclean.StringFunnel
 import edu.libsys.entity.Author
 import org.apache.spark.sql.SparkSession
 
 object AuthorStats {
-
-  //create a bloomFilter of string for removing duplicated authors
-  val expectedInsertions: Long = 830903
-  val stringFunnel: StringFunnel = new StringFunnel()
-  val bloomFilter: BloomFilter[String] = BloomFilter.create(stringFunnel, expectedInsertions)
-
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder()
@@ -26,45 +18,25 @@ object AuthorStats {
     val filePath = "/home/spark/Project/data/txt/id_author.txt"
     val delimiter = ","
 
-    //parse file to instances of IdAuthor
-    val idAuthorRDD = spark.sparkContext.textFile(filePath).map(line => {
+    //parse file to list of tuple
+    val authorIDTupleList = spark.sparkContext.textFile(filePath).map(line => {
       val tokens = line.split(delimiter).map(_.trim)
-      IdAuthor(tokens(0), tokens(1))
+      (tokens(1) -> tokens(0))
     })
-    idAuthorRDD.cache()
+    authorIDTupleList.cache()
+    println(authorIDTupleList.count())
 
-    //get names of unique author create instances of Author
-    val authorRDD = idAuthorRDD.map(_.authorName).filter(isUniqueAuthor).map(name => {
-      new Author(name)
+    //create list of tuple(s) like authorName -> paperId
+    val authorStats = authorIDTupleList.groupByKey.collect
+
+    //traversal list, save authors to database
+    authorStats.foreach(tuple => {
+      //println(tuple)
+      //println(tuple._2.toList.size)
+      authorDao.addAuthor(new Author(tuple._1, tuple._2.toArray.length))
     })
-    authorRDD.cache()
 
-    println(idAuthorRDD.count())
-    println(authorRDD.count())
-
-    //traversal authorNameArray, count the amount of works of each author
-    authorRDD.foreach(author => {
-      var workCount = 0
-      idAuthorRDD.foreach(idAuthor => {
-        if (idAuthor.authorName.equals(author.getName))
-          workCount += workCount
-      })
-      author.setWorkCount(workCount)
-      authorDao.addAuthor(author)
-    })
     //stop work
     spark.stop()
   }
-
-  def isUniqueAuthor(name: String): Boolean = {
-    val isUnique = !bloomFilter.mightContain(name)
-    if (isUnique) {
-      bloomFilter.put(name)
-    }
-    isUnique
-  }
-
-  //case class
-  case class IdAuthor(paperId: String, authorName: String)
-
 }
